b9937de405f2f9b49f1752504cc6fd716cdb030f
Wes Bos <wesbos@gmail.com>
2019-03-19 14:13:55 -0400

    Every 30 mins, not every 30 seconds

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { runCron } from './scraper';
 
-cron.schedule('*/30 * * * * *', () => {
+cron.schedule('0,30 * * * *', () => {
   console.log('⏲️ RUNNING THE CRON');
   runCron();
 });
